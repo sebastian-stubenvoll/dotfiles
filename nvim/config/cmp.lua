e5e2b07b6c089491720d0963df3dbfc3e12e103b
Sebastian Stubenvoll <sebastian.stubenvoll@gmail.com>
2023-05-02 23:09:47 +0200

    allow tab to expand lsp snippets

--- nvim/config/cmp.lua
+++ nvim/config/cmp.lua
@@ -29,18 +29,19 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(-4),
         ['<C-g>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.abort(),
-        -- confirm selection
-        ["<Tab>"] = cmp.mapping(
-          function(fallback)
-            cmp_ultisnips_mappings.compose { "expand", "jump_forwards" }(fallback)
-          end,
-          { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
-        ),
+        ['<Tab>'] = function(fallback)
+            local entry = cmp.get_selected_entry()
+            if entry and entry.source.name == 'nvim_lsp' then
+                cmp.confirm()
+            else
+                cmp_ultisnips_mappings.compose { "expand", "jump_forwards" }(fallback)
+            end
+        end,
         ["<S-Tab>"] = cmp.mapping(
-          function(fallback)
-            cmp_ultisnips_mappings.compose { "expand", "jump_backwards" }(fallback)
-          end,
-          { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+            function(fallback)
+                cmp_ultisnips_mappings.compose { "jump_backwards" }(fallback)
+            end,
+            { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
         ),
 
         -- navigate items on the list
@@ -53,14 +54,13 @@ cmp.setup({
         { name = 'path' },
         { name = 'buffer' },
         { name = 'latex_symbols' },
-        })
+    })
 })
 
 cmp.setup.cmdline({ '/', '?' }, {
     mapping = {
         ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-j>'] = cmp.mapping.select_next_item(),
-        ['<Tab>'] = cmp.mapping.confirm(),
     },
     sources = {
         { name = 'buffer' }
@@ -71,7 +71,6 @@ cmp.setup.cmdline(':', {
     mapping = {
         ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-j>'] = cmp.mapping.select_next_item(),
-        ['<Tab>'] = cmp.mapping.confirm(),
     },
     sources = cmp.config.sources({
         { name = 'path' }
